3b4d14549f4c513636f0611786708191cd35543a
bea-75 <beatrizesouthern75@gmail.com>
2024-03-08 10:23:31 -0800

    add active class to nav links

--- routes/index.js
+++ routes/index.js
@@ -4,19 +4,19 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) 
 {
-  res.render('index', { title: 'Home' });
+  res.render('index', { title: 'Home', active_page: 'home' });
 });
 
 /* GET volunteer hub page. */
 router.get('/volunteer-hub', function(req, res, next) 
 {
-  res.render('hub', { title: 'hub' });
+  res.render('hub', { title: 'hub', active_page: 'hub' });
 });
 
 /* GET UGC page. */
 router.get('/volunteer-experiences', function(req, res, next) 
 {
-  res.render('experience', { title: 'experience' });
+  res.render('experience', { title: 'experience', active_page: 'experience' });
 });
 
 router.get('/post-submission', function(req, res, next) 
